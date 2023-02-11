fbf34dd51b9e539c21b915d56ba8ab41836ae381
nakflix <123237114+nakflix@users.noreply.github.com>
2023-02-11 12:41:44 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -62,7 +62,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("ᴀᴅᴍɪɴ ɪs ᴡᴏʀᴋɪɴɢ ⚒ ᴏɴ ʏᴏᴜʀ ʀᴇϙᴜᴇsᴛ ᴋɪɴᴅʟʏ ʀᴇϙᴜᴇsᴛ ᴀɢᴀɪɴ ɪɴ ᴀ ғᴇᴡ ᴍɪɴᴜᴛᴇs.. 👌", show_alert=True)
+        return await query.answer("okda", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -661,7 +661,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer('Sᴜᴘᴘᴏʀᴛ TᴀᴍɪʟᴀɴBᴏᴛsZ')
+    await query.answer('Sᴜᴘᴘᴏʀᴛ ɴᴀᴋғʟɪx ᵗᵛ—͟͟͞͞𖣘')
 
 
 async def auto_filter(client, msg, spoll=False):
