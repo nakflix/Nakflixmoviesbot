2b02a8d7c4ecb0555de0f06883d913567256a49c
nakflix <123237114+nakflix@users.noreply.github.com>
2023-06-24 11:28:40 +0300

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Alpha3720")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Alpha_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001870385542'))
